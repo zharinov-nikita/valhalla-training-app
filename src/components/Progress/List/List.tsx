211ab856bf3158e440f1b0443e521fcb6c998483
Nikita <zharinov.nicita@gmail.com>
2022-07-02 14:34:44 +0700

    [feature/progress] listData - данные для отрисовки компонента item через map

--- src/components/Progress/List/List.tsx
+++ src/components/Progress/List/List.tsx
@@ -1,15 +1,14 @@
 import { FC } from 'react'
 import css from './List.module.scss'
 import Item from '../Item/Item'
+import { listData } from './list.data'
 
 const List: FC = () => {
   return (
     <div className={css.list}>
-      <Item option={{ name: 'Подтягивания', value: 15, time: 60 }} />
-      <Item option={{ name: 'Отжимания', value: 58, time: 120 }} />
-      <Item option={{ name: 'Пружина', value: 60, time: 103 }} />
-      <Item option={{ name: 'Выпрыгивания', value: 60, time: 103 }} />
-      <Item option={{ name: 'Пресс', value: 60, time: 80 }} />
+      {listData.map(({ option }) => (
+        <Item option={option} />
+      ))}
     </div>
   )
 }
---
+++ src/components/Progress/List/list.data.tsx
@@ -0,0 +1,9 @@
+import { ItemPropsType } from '../Item/Item'
+
+export const listData: ItemPropsType[] = [
+  { option: { name: 'Подтягивания', value: 15, time: 60 } },
+  { option: { name: 'Отжимания', value: 58, time: 120 } },
+  { option: { name: 'Пружина', value: 60, time: 103 } },
+  { option: { name: 'Выпрыгивания', value: 60, time: 103 } },
+  { option: { name: 'Пресс', value: 60, time: 80 } },
+]
