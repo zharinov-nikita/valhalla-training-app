d00778b8e8b3178c0f786f93fb03cd6a459af52c
Nikita <zharinov.nicita@gmail.com>
2022-07-02 17:19:05 +0700

    [feature/contentetable] useState - value, time

--- src/components/Progress/Item/Item.tsx
+++ src/components/Progress/Item/Item.tsx
@@ -1,27 +1,72 @@
 import { FieldTimeOutlined, TrophyOutlined } from '@ant-design/icons'
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import css from './Item.module.scss'
 
 export type ItemPropsType = {
   option: {
     name: string
-    value: number
-    time: number
+    value: number | string
+    time: number | string
+    level?: 'low' | 'mid' | 'high'
   }
 }
 
 const Item: FC<ItemPropsType> = ({ option }) => {
+  const [level, setLevel] = useState<'low' | 'mid' | 'high'>('low')
+  const [value, setValue] = useState<string>('')
+  const [time, setTime] = useState<string>('')
+
+  const onChangeValue = (e: { target: HTMLInputElement }) => {
+    if (!isNaN(Number(e.target.value)) && Number(e.target.value) <= 999) {
+      setValue(String(e.target.value))
+    }
+  }
+
+  useEffect(() => {
+    if (Number(value) <= 10) {
+      setLevel('low')
+    }
+
+    if (Number(value) >= 14 && Number(value) <= 16) {
+      setLevel('mid')
+    }
+
+    if (Number(value) >= 16) {
+      setLevel('high')
+    }
+  }, [value])
+
+  const onChangeTime = (e: { target: HTMLInputElement }) => {
+    if (!isNaN(Number(e.target.value)) && Number(e.target.value) <= 9999) {
+      setTime(String(e.target.value))
+    }
+  }
+
   return (
     <div className={css.item}>
       <div className={css.name}>{option.name}</div>
 
       <div className={css.list}>
-        <div className={css.value}>
-          {option.value}
+        <div className={css.value} data-level={level}>
+          <input
+            type="text"
+            value={value}
+            onChange={onChangeValue}
+            min={0}
+            max={200}
+            required
+          />
           <TrophyOutlined />
         </div>
         <div className={css.value}>
-          {option.time}
+          <input
+            type="text"
+            value={time}
+            onChange={onChangeTime}
+            min={0}
+            max={36000}
+            required
+          />
           <FieldTimeOutlined />
         </div>
       </div>
--- src/components/Progress/List/list.data.tsx
+++ src/components/Progress/List/list.data.tsx
@@ -1,9 +1,9 @@
 import { ItemPropsType } from '../Item/Item'
 
 export const listData: ItemPropsType[] = [
-  { option: { name: 'Подтягивания', value: 15, time: 60 } },
-  { option: { name: 'Отжимания', value: 58, time: 120 } },
-  { option: { name: 'Пружина', value: 60, time: 103 } },
-  { option: { name: 'Выпрыгивания', value: 60, time: 103 } },
-  { option: { name: 'Пресс', value: 60, time: 80 } },
+  { option: { name: 'Подтягивания', value: 15, time: 60, level: 'low' } },
+  { option: { name: 'Отжимания', value: 58, time: 120, level: 'high' } },
+  { option: { name: 'Пружина', value: 60, time: 103, level: 'high' } },
+  { option: { name: 'Выпрыгивания', value: 60, time: 103, level: 'high' } },
+  { option: { name: 'Пресс', value: 60, time: 80, level: 'high' } },
 ]
