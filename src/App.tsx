ae65f476bbef5310feb61efa937fae5fd61cbb3f
Nikita <zharinov.nicita@gmail.com>
2022-07-03 17:01:37 +0700

    [feature/component-exercise] Exierciese component отрисовка по условию

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import React from 'react'
-import Menu from './components/Menu/Menu'
-import Progress from './components/Progress/Progress'
-import Pulse from './components/Pulse/Pulse'
-import User from './components/User/User'
-import Workout from './components/Workout/Workout'
+import Exercise from './components/Exercise/Exercise'
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <div className="wrapper">
-          <Workout />
-          <Menu />
-          <User option={{ info: 'Жаринов Данил', value: 1200 }} />
-          <Progress />
-          <Pulse />
+          <Exercise />
         </div>
       </div>
     </div>
--- src/components/Workout/Workout.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react'
-import List from './List/List'
-
-const Workout: FC = () => {
-  return <List />
-}
-
-export default Workout
