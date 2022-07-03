c3e87958852bea7ede478aa1fbea4da87fd3c11c
Nikita <zharinov.nicita@gmail.com>
2022-07-03 09:06:20 +0700

    [feature/component-workout] Workout component, Workout modules scss

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Menu from './components/Menu/Menu'
 import Progress from './components/Progress/Progress'
 import Pulse from './components/Pulse/Pulse'
 import User from './components/User/User'
+import Workout from './components/Workout/Workout'
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <div className="wrapper">
+          <Workout />
           <Menu />
           <User option={{ info: 'Жаринов Данил', value: 1200 }} />
           <Progress />
---
+++ src/components/Workout/Workout.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+const Workout: FC = () => {
+  return <div>Workout</div>
+}
+
+export default Workout
