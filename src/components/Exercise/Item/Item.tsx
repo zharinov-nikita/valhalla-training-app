35551ba6ed091946791f4ec88bcc9ee32b575dab
Nikita <zharinov.nicita@gmail.com>
2022-07-03 18:10:40 +0700

    [feature/component-exercise] ExerciseItem - ItemPropsType типы для компонента

--- src/components/Exercise/Item/Item.tsx
+++ src/components/Exercise/Item/Item.tsx
@@ -1,57 +1,47 @@
 import { ExclamationCircleOutlined, FieldTimeOutlined } from '@ant-design/icons'
-import { FC } from 'react'
+import { FC, ReactNode } from 'react'
 import css from './Item.module.scss'
 
-const Item: FC = () => {
+export type ItemPropsType = {
+  props: {
+    _id: number | string
+    queue: number
+    title: string
+    description?: string
+    tags: Array<{ name: string | number; icon?: ReactNode }>
+  }
+}
+
+const Item: FC<ItemPropsType> = ({ props }) => {
   return (
-    <div className={css.list}>
-      {[
-        { id: 1, value: 'Подтягивания' },
-        { id: 2, value: 'Отжимания' },
-        { id: 3, value: 'Пружина' },
-      ].map((item) => (
-        <div className={css.item} key={item.id}>
-          <div className={css.body}>
-            <div className={css.left}>{item.id}</div>
-            <div className={css.right}>
-              <div className={css.header}>
-                <div className={css.info}>
-                  <div className={css.title}>{item.value}</div>
-                  <div className={css.description}>
-                    Подтягивания - хват обычный, дальше какой-то текст.
-                  </div>
-                </div>
-                <div className={css.btn}>
-                  <span>Завершить</span>
-                  <span>
-                    <ExclamationCircleOutlined />
-                  </span>
-                </div>
-              </div>
+    <div className={css.item}>
+      <div className={css.body}>
+        <div className={css.left}>{props.queue}</div>
+        <div className={css.right}>
+          <div className={css.header}>
+            <div className={css.info}>
+              <div className={css.title}>{props.title}</div>
+              {props.description && (
+                <div className={css.description}>{props.description}</div>
+              )}
             </div>
-          </div>
-          <div className={css.footer}>
-            <div className={css.tag}>
-              <span>Количество: 16</span>
+            <div className={css.btn}>
+              <span>Завершить</span>
               <span>
-                <FieldTimeOutlined />
-              </span>
-            </div>
-            <div className={css.tag}>
-              <span>Подход: 1</span>
-              <span>
-                <FieldTimeOutlined />
-              </span>
-            </div>
-            <div className={css.tag}>
-              <span>Отыдых: 60</span>
-              <span>
-                <FieldTimeOutlined />
+                <ExclamationCircleOutlined />
               </span>
             </div>
           </div>
         </div>
-      ))}
+      </div>
+      <div className={css.footer}>
+        {props.tags.map(({ icon, name }) => (
+          <div className={css.tag} key={name}>
+            <span>{name}</span>
+            {icon && <span>{icon}</span>}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
