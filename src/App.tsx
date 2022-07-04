7ec458ced4b62275a79f731e9bbeb1b4c6c5cf2f
Nikita <zharinov.nicita@gmail.com>
2022-07-04 13:44:20 +0700

    [global-css] variables, reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import { FC } from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import Day from './pages/Day/Day'
-import Exercise from './pages/Exercise/Exercise'
 
 const App: FC = () => {
   return (
-    <div className="App">
-      <div className="container">
-        <div className="wrapper">
-          <Routes>
-            <Route path="day" element={<Day />} />
-            <Route path="exercise/:_id" element={<Exercise />} />
-            <Route path="*" element={<Navigate to="/day" replace />} />
-          </Routes>
-        </div>
+    <div className="app">
+      <div className="app__container">
+        <div className="app__wrapper">s</div>
       </div>
     </div>
   )
