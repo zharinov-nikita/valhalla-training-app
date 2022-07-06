431f3fabe91d8c4d17480ba38be28f46f5e6b8f8
Nikita <zharinov.nicita@gmail.com>
2022-07-06 11:03:57 +0700

    [feature/completed] periodService, findById, findByIdAndUpdate

--- src/pages/Cycle/Cycle.tsx
+++ src/pages/Cycle/Cycle.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react'
 import { useLocation } from 'react-router-dom'
 import Info from '../../components/Info/Info'
+import {
+  useFindByIdAndUpdateMutation,
+  useFindByIdQuery,
+} from '../../redux/cycle/cycle.service'
 import { useCycleQuery } from '../../redux/service'
 import css from './Cycle.module.scss'
 
 const Cycle: FC = () => {
   const { search } = useLocation()
-  const { isError, isLoading, data } = useCycleQuery(search)
+  const { isError, isLoading, data } = useFindByIdQuery(search)
+  const [update, {}] = useFindByIdAndUpdateMutation()
   return (
     <div className={css.list}>
       {isLoading && 'Загрузка...'}
@@ -20,7 +25,13 @@ const Cycle: FC = () => {
               title: item.title,
               description: item.description,
               status: item.status,
-              progress: 30,
+              progress: item.status === 'Завершено' ? 100 : 0,
+              onClick: () =>
+                update({
+                  ...item,
+                  status:
+                    item.status === 'Завершено' ? 'В работе' : 'Завершено',
+                }),
             }}
           />
         ))}
