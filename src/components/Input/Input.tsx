c2a569ca9b4c5aa5bf2dd1959add8372d77f4e1f
Nikita <zharinov.nicita@gmail.com>
2022-07-06 13:31:55 +0700

    [feature/input] Styles

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 import css from './Input.module.scss'
 
 const Input: FC = () => {
-  return <input type="text" className={css.input} value={'sdfa'} />
+  return <input type="text" className={css.input} placeholder={'Placeholder'} />
 }
 
 export default Input
