8d7851df7bac1fc690ffc15c601708da3ac58d9f
Nikita <zharinov.nicita@gmail.com>
2022-07-06 10:54:00 +0700

    [feature/completed] connect store dayApi

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,18 +2,21 @@ import { configureStore } from '@reduxjs/toolkit'
 import { api } from './service'
 import { exerciseApi } from './exercise/exercise.service'
 import { workoutApi } from './workout/workout.service'
+import { dayApi } from './day/day.service'
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     [exerciseApi.reducerPath]: exerciseApi.reducer,
     [workoutApi.reducerPath]: workoutApi.reducer,
+    [dayApi.reducerPath]: dayApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       api.middleware,
       exerciseApi.middleware,
-      workoutApi.middleware
+      workoutApi.middleware,
+      dayApi.middleware
     ),
 })
 
