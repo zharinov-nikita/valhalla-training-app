00b1f1758825bb93a0a2a434be39c70e69db3b22
Nikita <zharinov.nicita@gmail.com>
2022-07-07 10:02:25 +0700

    [feature/pages-redux] List styles, defalult props

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -3,12 +3,23 @@ import css from './List.module.scss'
 
 export type ListPropsType = {
   props: {
-    children: ReactNode
+    gap: 12 | 14 | 16 | 18
+    children?: ReactNode
   }
 }
 
 const List: FC<ListPropsType> = ({ props }) => {
-  return <div className={css.list}>{props.children}</div>
+  return (
+    <div className={css.list} data-gap={props.gap}>
+      {props.children}
+    </div>
+  )
+}
+
+List.defaultProps = {
+  props: {
+    gap: 12,
+  },
 }
 
 export default List
