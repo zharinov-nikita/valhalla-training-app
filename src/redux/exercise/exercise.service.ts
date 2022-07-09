21d561ecfab5d748bb8cf3efc227f8b2b8156e91
Nikita <zharinov.nicita@gmail.com>
2022-07-09 12:42:44 +0700

    [feature/page-exercise] Service

--- src/redux/exercise/exercise.service.ts
+++ src/redux/exercise/exercise.service.ts
@@ -1,5 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+// API
+const apiBaseUrl = process.env['REACT_APP_API_BASE_URL']
+const apiKey = process.env['REACT_APP_API_KEY']
+// API
+
 export type ExerciseType = {
   _id: string
   title: string
@@ -20,16 +25,24 @@ export type ExerciseCreateType = {
 export const exerciseApi = createApi({
   tagTypes: ['Exercise'],
   reducerPath: 'exerciseApi',
-  baseQuery: fetchBaseQuery({ baseUrl: process.env['REACT_APP_API_BASE_URL'] }),
+  baseQuery: fetchBaseQuery({ baseUrl: apiBaseUrl }),
   endpoints: (builder) => ({
-    findById: builder.query<ExerciseType[], string>({
-      query: (param) => `/exercise/${param}`,
+    findByField: builder.query<ExerciseType[], string>({
+      query: (_id) => ({
+        url: `/exercise?dayId=${_id}`,
+        headers: {
+          'api-key': apiKey,
+        },
+      }),
       providesTags: ['Exercise'],
     }),
     create: builder.mutation<ExerciseType, ExerciseCreateType>({
       query: (exercise) => ({
         url: `/exercise`,
         method: 'POST',
+        headers: {
+          'api-key': apiKey,
+        },
         body: exercise,
       }),
       invalidatesTags: ['Exercise'],
@@ -38,7 +51,20 @@ export const exerciseApi = createApi({
       query: (exercise) => ({
         url: `/exercise/${exercise._id}`,
         method: 'PATCH',
+        headers: {
+          'api-key': apiKey,
+        },
         body: exercise,
+      }),
+      invalidatesTags: ['Exercise'],
+    }),
+    findByIdAndDelete: builder.mutation<ExerciseType, ExerciseType>({
+      query: (exercise) => ({
+        url: `/exercise/${exercise._id}`,
+        headers: {
+          'api-key': apiKey,
+        },
+        method: 'DELETE',
       }),
       invalidatesTags: ['Exercise'],
     }),
@@ -46,7 +72,8 @@ export const exerciseApi = createApi({
 })
 
 export const {
-  useFindByIdQuery,
+  useFindByFieldQuery,
   useCreateMutation,
   useFindByIdAndUpdateMutation,
+  useFindByIdAndDeleteMutation,
 } = exerciseApi
