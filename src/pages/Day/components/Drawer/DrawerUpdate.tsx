459dc55b144c8bf835ed031f723f1fcceaa7ec65
Nikita <zharinov.nicita@gmail.com>
2022-07-09 17:24:53 +0700

    [feature/useForm] useForm - Day DrawerUpdate

--- src/pages/Day/components/Drawer/DrawerUpdate.tsx
+++ src/pages/Day/components/Drawer/DrawerUpdate.tsx
@@ -8,6 +8,7 @@ import { useAppSelector } from '../../../../hooks/useAppSelector'
 import { drawerSlice } from '../../../../redux/drawer/drawer.slice'
 import { useFindByIdAndUpdateMutation } from '../../../../redux/day/day.service'
 import { daySlice } from '../../../../redux/day/day.slice'
+import { useForm } from '../../../../hooks/useForm'
 
 type ItemFormType = {
   id: number
@@ -23,6 +24,9 @@ const DrawerUpdate: FC = () => {
   const { formUpdate } = useAppSelector((state) => state.day)
   const { updateFormUpdate, clearFormUpdate } = daySlice.actions
   const { hide } = drawerSlice.actions
+
+  const { title, description, status } = formUpdate
+  const { disabled } = useForm([title, description, status])
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     dispatch(
