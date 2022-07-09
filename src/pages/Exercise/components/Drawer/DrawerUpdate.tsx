4aafb30b55640e54216eec9d54154142a5fdba01
Nikita <zharinov.nicita@gmail.com>
2022-07-09 17:37:03 +0700

    [feature/useForm] useForm - Exercise DrawerUpdate

--- src/pages/Exercise/components/Drawer/DrawerUpdate.tsx
+++ src/pages/Exercise/components/Drawer/DrawerUpdate.tsx
@@ -7,6 +7,7 @@ import List from '../../../../components/List/List'
 import Textarea from '../../../../components/Textarea/Textarea'
 import { useAppDispatch } from '../../../../hooks/useAppDispatch'
 import { useAppSelector } from '../../../../hooks/useAppSelector'
+import { useForm } from '../../../../hooks/useForm'
 import { drawerSlice } from '../../../../redux/drawer/drawer.slice'
 import { useFindByIdAndUpdateMutation } from '../../../../redux/exercise/exercise.service'
 import { exerciseSlice } from '../../../../redux/exercise/exercise.slice'
@@ -17,6 +18,7 @@ const DrawerUpdate: FC = () => {
 
   const [update, { isSuccess }] = useFindByIdAndUpdateMutation()
   const dispatch = useAppDispatch()
+
   const { formUpdate } = useAppSelector((state) => state.exercise)
   const {
     updateFormUpdate,
@@ -26,6 +28,12 @@ const DrawerUpdate: FC = () => {
     addOptionFormUpdate,
   } = exerciseSlice.actions
   const { hide } = drawerSlice.actions
+
+  const { title, description, status } = formUpdate
+  const options = formUpdate.option.map((item) => {
+    return item.title, item.value
+  })
+  const { disabled } = useForm([title, description, status, ...options])
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     dispatch(
@@ -129,6 +137,7 @@ const DrawerUpdate: FC = () => {
         props={{
           text: 'Обновить тренировку',
           block: true,
+          disabled,
           onClick: () => update({ ...formUpdate, workoutId }),
         }}
       />
