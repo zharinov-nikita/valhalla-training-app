15c7373d53fbf5534968e2a78d1d10c762bcefad
Nikita <zharinov.nicita@gmail.com>
2022-07-16 10:53:42 +0700

    feature/wokrout Approach app

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Cycle from './pages/Cycle/Cycle'
 import Day from './pages/Day/Day'
 import Workout from './pages/Workout/Workout'
 import Exercise from './pages/Exercise/Exercise'
-import Approach from './components/Approach/Approach'
 
 const App: FC = () => {
   return (
@@ -16,52 +15,6 @@ const App: FC = () => {
       <div className="app__container">
         <Header />
         <div className="app__wrapper">
-          <Approach
-            props={[
-              {
-                id: '1',
-                comleted: true,
-                quantity: 16.5,
-                rest: 20,
-              },
-              {
-                id: '2',
-                comleted: false,
-                quantity: 14,
-                rest: 20,
-              },
-              {
-                id: '3',
-                comleted: false,
-                quantity: 18,
-                rest: 20,
-              },
-              {
-                id: '4',
-                comleted: false,
-                quantity: 18,
-                rest: 20,
-              },
-              {
-                id: '5',
-                comleted: false,
-                quantity: 14,
-                rest: 20,
-              },
-              {
-                id: '6',
-                comleted: false,
-                quantity: 18,
-                rest: 20,
-              },
-              {
-                id: '7',
-                comleted: false,
-                quantity: 18,
-                rest: 20,
-              },
-            ]}
-          />
           <Routes>
             <Route path="/plan" element={<Plan />} />
             <Route path="/period" element={<Period />} />
