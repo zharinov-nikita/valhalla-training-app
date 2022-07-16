6692563041c73881859517b721f062f5fd544f3e
Nikita <zharinov.nicita@gmail.com>
2022-07-16 15:03:26 +0700

    feature/menu Menu slice

---
+++ src/redux/menu/menu.slice.ts
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export type MenuStateType = {
+  visible: boolean
+}
+
+const initialState: MenuStateType = {
+  visible: false,
+}
+
+export const menuSlice = createSlice({
+  name: 'menu',
+  initialState,
+  reducers: {
+    show(state: MenuStateType) {
+      state.visible = true
+    },
+    hide(state: MenuStateType) {
+      state.visible = false
+    },
+  },
+})
+
+export const { show, hide } = menuSlice.actions
+export default menuSlice.reducer
