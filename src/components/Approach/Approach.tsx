be365cd63469936547018b69b0bad3439f2223ab
Nikita <zharinov.nicita@gmail.com>
2022-07-16 10:52:17 +0700

    feature/wokrout Approach complenent, styles

---
+++ src/components/Approach/Approach.tsx
@@ -0,0 +1,29 @@
+import React, { FC, useState } from 'react'
+import css from './Approach.module.scss'
+
+export type ApproachPropsType = {
+  props: Array<{
+    id: string
+    comleted: boolean
+    quantity: number
+    rest: number
+  }>
+}
+
+const Approach: FC<ApproachPropsType> = ({ props }) => {
+  return (
+    <div className={css.list}>
+      {props &&
+        props.map((item) => (
+          <React.Fragment key={item.id}>
+            <div className={css.item} data-completed={item.comleted}>
+              <div className={css.header}>{item.quantity} раз</div>
+              <div className={css.footer}>{item.rest} сек</div>
+            </div>
+          </React.Fragment>
+        ))}
+    </div>
+  )
+}
+
+export default Approach
