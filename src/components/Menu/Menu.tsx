9bec30fd3a2221557827f929d41059cb9f51ec1a
Nikita <zharinov.nicita@gmail.com>
2022-07-16 15:53:05 +0700

    feature/menu useEffect route link to

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,32 +1,41 @@
 import { AimOutlined } from '@ant-design/icons'
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import { useAppDispatch } from '../../hooks/useAppDispatch'
 import { useAppSelector } from '../../hooks/useAppSelector'
+import { menuSlice } from '../../redux/menu/menu.slice'
 import css from './Menu.module.scss'
 
 const Menu: FC = () => {
+  const { pathname } = useLocation()
+  const [click, setClick] = useState<number>(1)
+  const dispatch = useAppDispatch()
+  const { hide } = menuSlice.actions
   const { visible } = useAppSelector((state) => state.menu)
+
+  useEffect(() => {
+    if (click) {
+      dispatch(hide())
+    }
+  }, [click])
+
   return (
     <div className={css.menu} data-visible={visible}>
       <div className={css.wrapper}>
         <div className={css.list}>
-          <div className={css.item}>
+          <Link
+            to={'/period/?planId=62d2776c5154ed09fec00744'}
+            onClick={() => setClick(Number(Date.now()))}
+            className={css.item}
+          >
             <div className={css.icon}>
               <AimOutlined />
             </div>
-            <div className={css.text}>Hello world</div>
-          </div>
-          <div className={css.item}>
-            <div className={css.icon}>
-              <AimOutlined />
+            <div className={css.text}>
+              <span>Моя неделя</span>
+              <span className={css.tag}>beta</span>
             </div>
-            <div className={css.text}>Hello world</div>
-          </div>
-          <div className={css.item}>
-            <div className={css.icon}>
-              <AimOutlined />
-            </div>
-            <div className={css.text}>Hello world</div>
-          </div>
+          </Link>
         </div>
       </div>
     </div>
