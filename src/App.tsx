e4799ad947af141258642f74bf77bb0d02a2bd4d
Nikita <zharinov.nicita@gmail.com>
2022-07-17 12:25:28 +0700

    feature/ip check cookie token

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 
 import Header from './components/Header/Header'
@@ -10,25 +10,33 @@ import Workout from './pages/Workout/Workout'
 import Exercise from './pages/Exercise/Exercise'
 import Menu from './components/Menu/Menu'
 import Modal from './components/Modal/Modal'
+import { useCookies } from 'react-cookie'
 
 const App: FC = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(['token'])
+  const accessToken = process.env['REACT_APP_TOKEN']
+
   return (
     <div className="app">
       <div className="app__container">
         <Modal />
-        <Header />
-        <Menu />
-        <div className="app__wrapper">
-          <Routes>
-            <Route path="/plan" element={<Plan />} />
-            <Route path="/period" element={<Period />} />
-            <Route path="/cycle" element={<Cycle />} />
-            <Route path="/day" element={<Day />} />
-            <Route path="/workout" element={<Workout />} />
-            <Route path="/exercise" element={<Exercise />} />
-            <Route path="*" element={<Navigate to="/plan" replace />} />
-          </Routes>
-        </div>
+        {cookies.token === accessToken && (
+          <>
+            <Header />
+            <Menu />
+            <div className="app__wrapper">
+              <Routes>
+                <Route path="/plan" element={<Plan />} />
+                <Route path="/period" element={<Period />} />
+                <Route path="/cycle" element={<Cycle />} />
+                <Route path="/day" element={<Day />} />
+                <Route path="/workout" element={<Workout />} />
+                <Route path="/exercise" element={<Exercise />} />
+                <Route path="*" element={<Navigate to="/plan" replace />} />
+              </Routes>
+            </div>
+          </>
+        )}
       </div>
     </div>
   )
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,25 +1,63 @@
-import { FC, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
+import { useCookies } from 'react-cookie'
 import Button from '../Button/Button'
 import Input from '../Input/Input'
 import css from './Modal.module.scss'
 
+import AccessPrivate from './AccessPrivate'
+import AccessOpen from './AccessOpen'
+
 const Modal: FC = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(['token'])
+
+  const accessToken = process.env['REACT_APP_TOKEN']
+
+  const [text, setText] = useState<string>('Valhalla ID')
   const [token, setToken] = useState<string>('')
+  const [disabled, setDisabled] = useState<boolean>(true)
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setToken(e.currentTarget.value)
+
+  useEffect(() => {
+    if (token === accessToken) {
+      setDisabled(false)
+      setText('Окей, летсгоууу!')
+    }
+    if (token !== accessToken) {
+      setDisabled(true)
+      setText('Valhalla ID')
+    }
+  }, [token])
+
+  const onClick = () => setCookie('token', accessToken)
+
   return (
-    <div className={css.modal}>
+    <div
+      className={css.modal}
+      data-token={cookies.token === accessToken ? true : false}
+    >
       <div className={css.container}>
         <div className={css.wrapper}>
-          <div className={css.header}></div>
+          <div className={css.header}>
+            {disabled ? <AccessPrivate /> : <AccessOpen />}
+          </div>
           <div className={css.body}>
             <Input
-              props={{ name: 'token', placeholder: 'token', value: 'token' }}
+              props={{
+                name: 'token',
+                placeholder: 'Vallhalla token',
+                value: token,
+                onChange,
+              }}
             />
             <Button
               props={{
                 block: true,
-                text: 'Окей, летсгоу',
+                text,
                 type: 'primary',
-                disabled: true,
+                disabled,
+                onClick,
               }}
             />
           </div>
