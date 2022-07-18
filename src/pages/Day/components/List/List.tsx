525e093b79312a46e0ee52d9dc69c0101241fdbf
Nikita <zharinov.nicita@gmail.com>
2022-07-18 13:57:20 +0700

    feature/body-fixed List DAy

--- src/pages/Day/components/List/List.tsx
+++ src/pages/Day/components/List/List.tsx
@@ -6,6 +6,8 @@ import Info from '../../../../components/Info/Info'
 import { useAppDispatch } from '../../../../hooks/useAppDispatch'
 import { useAppSelector } from '../../../../hooks/useAppSelector'
 import { drawerSlice } from '../../../../redux/drawer/drawer.slice'
+import { appSlice } from '../../../../redux/
+app/app.slice'
 import {
   useFindByIdAndUpdateMutation,
   useFindByIdAndDeleteMutation,
@@ -30,7 +32,8 @@ const List: FC = () => {
   const dispatch = useAppDispatch()
   const { show } = drawerSlice.actions
   const { action } = useAppSelector((state) => state.drawer)
-
+const { fix } 
+= appSlice.actions
   if (isLoading) {
     return <>Загрузка...</>
   }
@@ -64,6 +67,7 @@ const List: FC = () => {
               onClickDrawer: () => {
                 dispatch(updateFormUpdate(item))
                 dispatch(show('update'))
+                dispatch(fix())
               },
               onClickDelete: () => findByIdAndDelete(item),
             }}
