a1a095bfcaaf2965b0f6b318d4778b57b57a7a12
Nikita <zharinov.nicita@gmail.com>
2022-07-18 13:53:54 +0700

    feature/body-fixed List Cycle, Plan

--- src/pages/Cycle/components/List/List.tsx
+++ src/pages/Cycle/components/List/List.tsx
@@ -6,6 +6,7 @@ import Info from '../../../../components/Info/Info'
 import { useAppDispatch } from '../../../../hooks/useAppDispatch'
 import { useAppSelector } from '../../../../hooks/useAppSelector'
 import { drawerSlice } from '../../../../redux/drawer/drawer.slice'
+import { appSlice } from '../../../../redux/app/app.slice'
 import {
   useFindByIdAndUpdateMutation,
   useFindByIdAndDeleteMutation,
@@ -28,8 +29,11 @@ const List: FC = () => {
   const [findByIdAndUpdate, {}] = useFindByIdAndUpdateMutation()
   const [findByIdAndDelete, {}] = useFindByIdAndDeleteMutation()
   const dispatch = useAppDispatch()
+
   const { show } = drawerSlice.actions
   const { action } = useAppSelector((state) => state.drawer)
+
+  const { fix } = appSlice.actions
 
   if (isLoading) {
     return <>Загрузка...</>
@@ -64,6 +68,7 @@ const List: FC = () => {
               onClickDrawer: () => {
                 dispatch(updateFormUpdate(item))
                 dispatch(show('update'))
+                dispatch(fix())
               },
               onClickDelete: () => findByIdAndDelete(item),
             }}
