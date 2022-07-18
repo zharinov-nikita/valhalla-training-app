c6c98c12123bf4153a0a027c6a4410c0cdf412a8
Nikita <zharinov.nicita@gmail.com>
2022-07-18 13:45:25 +0700

    feature/body-fixed Plan, Period, Workout

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,12 @@ import Workout from './pages/Workout/Workout'
 import Exercise from './pages/Exercise/Exercise'
 import Menu from './components/Menu/Menu'
 import Modal from './components/Modal/Modal'
+import { useAppSelector } from './hooks/useAppSelector'
 
 const App: FC = () => {
+  const { fixed } = useAppSelector((state) => state.app)
   return (
-    <div className="app" data-theme={'dark'} data-fixed={true}>
+    <div className="app" data-theme={'dark'} data-fixed={fixed}>
       <div className="app__container">
         <Header />
         <Menu />
