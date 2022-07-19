74d823d939011e00f3b0a38bce8ad03e90f3fa28
Nikita <zharinov.nicita@gmail.com>
2022-07-19 17:58:41 +0700

    feature/user User pages, styles

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,19 +1,47 @@
 import React, { FC } from 'react'
+import css from './User.module.scss'
 import { Link } from 'react-router-dom'
-import { useAppSelector } from '../../hooks/useAppSelector'
 import { useFindQuery } from '../../redux/user/user.service'
+import Empty from '../../components/Empty/Empty'
+import Loader from '../../components/Loader/Loader'
 
 const User: FC = () => {
   const pollingInterval = Number(process.env['REACT_APP_POLLING_INTERVAL'])
   const { isError, isLoading, data } = useFindQuery('', { pollingInterval })
+
+  if (isLoading) {
+    return <Empty children={<Loader />} />
+  }
+
+  if (isError) {
+    return <Empty children={'Произошла ошибка'} />
+  }
+
+  if (data && data.length === 0) {
+    return <Empty children={'Упс 🌱'} />
+  }
+
   return (
-    <div>
-      <div></div>
+    <div className={css.user}>
       {data &&
         data.map((item) => (
           <React.Fragment key={item._id}>
-            <div>Привет, {item.name}</div>
-            <Link to={`/plan/?userId=${item._id}`}>План</Link>
+            <div className={css.info}>
+              <div className={css.header}>
+                <div className={css.left}>
+                  <div className={css.icon}>{item.name.split('')[0]}</div>
+                  <div className={css.data}>
+                    <div className={css.top}>{item.name}</div>
+                    <div className={css.buttom}>{item.login}</div>
+                  </div>
+                </div>
+                <div className={css.left}>
+                  <Link to={''} className={css.link}>
+                    Моя неделя
+                  </Link>
+                </div>
+              </div>
+            </div>
           </React.Fragment>
         ))}
     </div>
