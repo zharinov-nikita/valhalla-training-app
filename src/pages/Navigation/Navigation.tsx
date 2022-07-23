bef71bdca9184e33aa0ed31b004ff774724effc9
Nikita <zharinov.nicita@gmail.com>
2022-07-23 17:38:29 +0700

    feature/storyboock navigation page

---
+++ src/pages/Navigation/Navigation.tsx
@@ -0,0 +1,38 @@
+import {
+  SettingOutlined,
+  UserOutlined,
+  ZoomOutOutlined,
+} from '@ant-design/icons'
+import Navigation from '../../components/Navigation/Navigation'
+
+export default function () {
+  return (
+    <Navigation
+      props={{
+        list: [
+          {
+            id: 1,
+            to: '/user',
+            title: 'Профиль',
+            description: 'Страница, на которой находиться ваш профиль',
+            icon: <ZoomOutOutlined />,
+          },
+          {
+            id: 2,
+            to: '/plan',
+            title: 'Аккаунт',
+            description: 'Страница, на которой находиться ваш профиль',
+            icon: <UserOutlined />,
+          },
+          {
+            id: 3,
+            to: '/cycle',
+            title: 'Моя неделя',
+            description: 'Страница, на которой находиться ваш профиль',
+            icon: <SettingOutlined />,
+          },
+        ],
+      }}
+    />
+  )
+}
