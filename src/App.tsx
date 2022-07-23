f29b6046132b56d2d01b7e50ae32e6c46aa831fc
Nikita <zharinov.nicita@gmail.com>
2022-07-23 11:12:05 +0700

    feature/storybook appSlice - theme, postion, switchTheme, changePostion

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,15 @@
 import { FC } from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { useAppDispatch } from './hooks/useAppDispatch'
 import { useAppSelector } from './hooks/useAppSelector'
-import './scss/index.scss'
-
-import Header from './components/Header/Header'
-import Plan from './pages/Plan/Plan'
-import Period from './pages/Period/Period'
-import Cycle from './pages/Cycle/Cycle'
-import Day from './pages/Day/Day'
-import Workout from './pages/Workout/Workout'
-import Exercise from './pages/Exercise/Exercise'
-import Menu from './components/Menu/Menu'
-import User from './pages/User/User'
-import Login from './pages/Login/Login'
-import Token from './pages/Token/Token'
+import { appSlice } from './redux/app/app.slice'
+import './styles/index.scss'
 
 const App: FC = () => {
-  const { fixed, user } = useAppSelector((state) => state.app)
-
-  if (!user.token) {
-    return (
-      <div className="app" data-theme={'dark'} data-fixed={fixed}>
-        <Routes>
-          <Route path="/token" element={<Token />} />
-          <Route path="*" element={<Navigate to="/token" replace />} />
-        </Routes>
-      </div>
-    )
-  }
-
-  if (!user.login && !user.password) {
-    return (
-      <div className="app" data-theme={'dark'} data-fixed={fixed}>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<Navigate to="/login" replace />} />
-        </Routes>
-      </div>
-    )
-  }
-
+  const { theme, positon } = useAppSelector((state) => state.app)
   return (
-    <div className="app" data-theme={'dark'} data-fixed={fixed}>
-      <div className="app__container">
-        <Header />
-        <Menu />
-        <div className="app__wrapper">
-          <Routes>
-            <Route path="/user" element={<User />} />
-            <Route path="/plan" element={<Plan />} />
-            <Route path="/period" element={<Period />} />
-            <Route path="/cycle" element={<Cycle />} />
-            <Route path="/day" element={<Day />} />
-            <Route path="/workout" element={<Workout />} />
-            <Route path="/exercise" element={<Exercise />} />
-            <Route path="*" element={<Navigate to="/user" replace />} />
-          </Routes>
-        </div>
+    <div className="app" data-theme={theme} data-positon={positon}>
+      <div className="container">
+        <div className="wrapper">{theme}</div>
       </div>
     </div>
   )
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,61 +2,23 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit'
 
 // slice
 import appReducer from './app/app.slice'
-import headerReducer from './header/header.slice'
-import drawerReducer from './drawer/drawer.slice'
-import menuReducer from './menu/menu.slice'
-import planReducer from './plan/plan.slice'
-import periodReducer from './period/period.slice'
-import cycleReducer from './cycle/cycle.slice'
-import dayReducer from './day/day.slice'
-import workoutReducer from './workout/workout.slice'
-import exerciseReducer from './exercise/exercise.slice'
 
 // service
 import { userApi } from './user/user.service'
-import { planApi } from './plan/plan.service'
-import { periodApi } from './period/period.service'
-import { cycleApi } from './cycle/cycle.service'
-import { dayApi } from './day/day.service'
-import { workoutApi } from './workout/workout.service'
-import { exerciseApi } from './exercise/exercise.service'
 
 const rootReducer = combineReducers({
   // slice
   app: appReducer,
-  header: headerReducer,
-  drawer: drawerReducer,
-  menu: menuReducer,
-  plan: planReducer,
-  period: periodReducer,
-  cycle: cycleReducer,
-  day: dayReducer,
-  workout: workoutReducer,
-  exercise: exerciseReducer,
 
   // serivce
   [userApi.reducerPath]: userApi.reducer,
-  [planApi.reducerPath]: planApi.reducer,
-  [periodApi.reducerPath]: periodApi.reducer,
-  [cycleApi.reducerPath]: cycleApi.reducer,
-  [dayApi.reducerPath]: dayApi.reducer,
-  [workoutApi.reducerPath]: workoutApi.reducer,
-  [exerciseApi.reducerPath]: exerciseApi.reducer,
 })
 
 export const setupStore = () => {
   return configureStore({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(
-        userApi.middleware,
-        planApi.middleware,
-        periodApi.middleware,
-        cycleApi.middleware,
-        dayApi.middleware,
-        workoutApi.middleware,
-        exerciseApi.middleware
-      ),
+      getDefaultMiddleware().concat(userApi.middleware),
   })
 }
 
