10285d8abc8ef593a636847c155adcebd1560c0e
Nikita <zharinov.nicita@gmail.com>
2022-08-01 10:50:24 +0700

    feature/microfrontend user message input

--- src/components/Common/Input/Input.tsx
+++ src/components/Common/Input/Input.tsx
@@ -1,15 +1,27 @@
 import { FC, ChangeEventHandler, useState } from 'react'
+import List from '../List/List'
 import style from './Input.module.scss'
 
 export type InputPropsType = {
   value: string
   placeholder?: string
+  message?: string
+  type?: 'default' | 'error'
   onChange?: ChangeEventHandler<HTMLInputElement>
 }
 
-const Input: FC<InputPropsType> = ({ value, placeholder, onChange }) => {
+const Input: FC<InputPropsType> = ({ value, placeholder, message, type, onChange }) => {
   return (
-    <input className={style.input} value={value} placeholder={placeholder} onChange={onChange} />
+    <List className={style.block} gap={8}>
+      {message && <span className={style.message}>{message}</span>}
+      <input
+        className={style.input}
+        value={value}
+        placeholder={placeholder}
+        data-type={type}
+        onChange={onChange}
+      />
+    </List>
   )
 }
 
--- src/pages/User/components/Drawer/Drawer.tsx
+++ src/pages/User/components/Drawer/Drawer.tsx
@@ -4,7 +4,7 @@ import { useAppSelector } from '../../../../hooks/store/useAppSelector'
 import { userSlice } from '../../../../redux/user/user.slice'
 import Button from '../../../../components/Common/Button/Button'
 import CommonDrawer from '../../../../components/Common/Drawer/Drawer'
-import Input from '../../../../components/Common/Input/Input'
+import Input, { InputPropsType } from '../../../../components/Common/Input/Input'
 import List from '../../../../components/Common/List/List'
 import { useFindByIdAndUpdateMutation } from '../../../../redux/user/user.service'
 import { drawerSlice } from '../../../../redux/drawer/drawer.slice'
@@ -13,7 +13,9 @@ type keyType = 'firstname' | 'lastname' | 'role' | 'login' | 'password'
 
 const Drawer: FC = () => {
   const [key, setKey] = useState<keyType>('firstname')
-  const [findByIdAndUpdate, { isSuccess }] = useFindByIdAndUpdateMutation()
+  const [message, setMessage] = useState<string>('')
+  const [inputType, setInputType] = useState<InputPropsType['type']>('default')
+  const [findByIdAndUpdate, { isSuccess, isError }] = useFindByIdAndUpdateMutation()
   const { type } = useAppSelector((state) => state.drawer)
   const { data } = useAppSelector((state) => state.user)
   const dispatch = useAppDispatch()
@@ -25,20 +27,56 @@ const Drawer: FC = () => {
   useMemo(() => {
     if (type === 'Имя') {
       setKey('firstname')
+      setMessage('')
+      setInputType('default')
     }
     if (type === 'Фамилия') {
       setKey('lastname')
+      setMessage('')
+      setInputType('default')
     }
     if (type === 'Роль') {
       setKey('role')
+      setMessage('')
+      setInputType('default')
     }
     if (type === 'Логин') {
       setKey('login')
+      setMessage('')
+      setInputType('default')
     }
     if (type === 'Пароль') {
       setKey('password')
+      setMessage('')
+      setInputType('default')
     }
-  }, [type])
+  }, [type, isError])
+
+  useMemo(() => {
+    if (isError) {
+      setInputType('error')
+
+      if (type === 'Имя') {
+        setMessage('Ошибка: невалидное имя')
+      }
+      if (type === 'Фамилия') {
+        setMessage('Ошибка: невалидная фамилия')
+      }
+      if (type === 'Роль') {
+        setMessage('Ошибка: роль может быть "Спортсмен" или "Тренер"')
+      }
+      if (type === 'Логин') {
+        setMessage('Ошибка: такой логин уже существует')
+      }
+      if (type === 'Пароль') {
+        setMessage('Ошибка: невалидный пароль')
+      }
+    }
+    if (!isError) {
+      setMessage('')
+      setInputType('default')
+    }
+  }, [isError])
 
   useEffect(() => {
     if (isSuccess) {
@@ -52,6 +90,8 @@ const Drawer: FC = () => {
         <Input
           value={data[key]}
           placeholder={type}
+          message={message}
+          type={inputType}
           onChange={(e) => dispatch(changeData({ ...data, [key]: e.target.value }))}
         />
         <Button
