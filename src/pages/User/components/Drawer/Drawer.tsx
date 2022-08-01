f9b53778c7fe47216fefbcc781f4e455035438a0
Nikita <zharinov.nicita@gmail.com>
2022-08-01 11:12:14 +0700

    feature/microfrontend useDrawer

--- src/pages/User/components/Drawer/Drawer.tsx
+++ src/pages/User/components/Drawer/Drawer.tsx
@@ -1,88 +1,17 @@
-import { FC, useEffect, useMemo, useState } from 'react'
-import { useAppDispatch } from '../../../../hooks/store/useAppDispatch'
-import { useAppSelector } from '../../../../hooks/store/useAppSelector'
-import { userSlice } from '../../../../redux/user/user.slice'
+import { FC } from 'react'
 import Button from '../../../../components/Common/Button/Button'
 import CommonDrawer from '../../../../components/Common/Drawer/Drawer'
-import Input, { InputPropsType } from '../../../../components/Common/Input/Input'
+import Input from '../../../../components/Common/Input/Input'
 import List from '../../../../components/Common/List/List'
 import { useFindByIdAndUpdateMutation } from '../../../../redux/user/user.service'
-import { drawerSlice } from '../../../../redux/drawer/drawer.slice'
-
-type keyType = 'firstname' | 'lastname' | 'role' | 'login' | 'password'
+import { useDrawer } from './hooks/useDrawer'
 
 const Drawer: FC = () => {
-  const [key, setKey] = useState<keyType>('firstname')
-  const [message, setMessage] = useState<string>('')
-  const [inputType, setInputType] = useState<InputPropsType['type']>('default')
   const [findByIdAndUpdate, { isSuccess, isError }] = useFindByIdAndUpdateMutation()
-  const { type } = useAppSelector((state) => state.drawer)
-  const { data } = useAppSelector((state) => state.user)
-  const dispatch = useAppDispatch()
-
-  const { changeData } = userSlice.actions
-  const { hide } = drawerSlice.actions
-  const text = `Обновить ${type !== 'Фамилия' ? type?.toLowerCase() : 'фамилию'}`
-
-  useMemo(() => {
-    if (type === 'Имя') {
-      setKey('firstname')
-      setMessage('')
-      setInputType('default')
-    }
-    if (type === 'Фамилия') {
-      setKey('lastname')
-      setMessage('')
-      setInputType('default')
-    }
-    if (type === 'Роль') {
-      setKey('role')
-      setMessage('')
-      setInputType('default')
-    }
-    if (type === 'Логин') {
-      setKey('login')
-      setMessage('')
-      setInputType('default')
-    }
-    if (type === 'Пароль') {
-      setKey('password')
-      setMessage('')
-      setInputType('default')
-    }
-  }, [type, isError])
-
-  useMemo(() => {
-    if (isError) {
-      setInputType('error')
-
-      if (type === 'Имя') {
-        setMessage('Ошибка: невалидное имя')
-      }
-      if (type === 'Фамилия') {
-        setMessage('Ошибка: невалидная фамилия')
-      }
-      if (type === 'Роль') {
-        setMessage('Ошибка: роль может быть "Спортсмен" или "Тренер"')
-      }
-      if (type === 'Логин') {
-        setMessage('Ошибка: такой логин уже существует')
-      }
-      if (type === 'Пароль') {
-        setMessage('Ошибка: невалидный пароль')
-      }
-    }
-    if (!isError) {
-      setMessage('')
-      setInputType('default')
-    }
-  }, [isError])
-
-  useEffect(() => {
-    if (isSuccess) {
-      dispatch(hide())
-    }
-  }, [isSuccess])
+  const { key, inputType, message, text, type, data, disabled, handlerChangeData } = useDrawer(
+    isError,
+    isSuccess
+  )
 
   return (
     <CommonDrawer>
@@ -92,11 +21,11 @@ const Drawer: FC = () => {
           placeholder={type}
           message={message}
           type={inputType}
-          onChange={(e) => dispatch(changeData({ ...data, [key]: e.target.value }))}
+          onChange={handlerChangeData}
         />
         <Button
           block={true}
-          disabled={data[key].length === 0 ? true : false}
+          disabled={disabled}
           size="medium"
           text={text}
           color={{ type: 'fill', value: 'purple' }}
---
+++ src/pages/User/components/Drawer/hooks/useDrawer.ts
@@ -0,0 +1,89 @@
+import { useEffect, useMemo, useState } from 'react'
+import { InputPropsType } from '../../../../../components/Common/Input/Input'
+import { useAppDispatch } from '../../../../../hooks/store/useAppDispatch'
+import { useAppSelector } from '../../../../../hooks/store/useAppSelector'
+import { drawerSlice } from '../../../../../redux/drawer/drawer.slice'
+import { userSlice } from '../../../../../redux/user/user.slice'
+
+type keyType = 'firstname' | 'lastname' | 'role' | 'login' | 'password'
+
+export function useDrawer(isError: boolean, isSuccess: boolean) {
+  const { type } = useAppSelector((state) => state.drawer)
+  const [key, setKey] = useState<keyType>('firstname')
+  const [message, setMessage] = useState<string>('')
+  const [inputType, setInputType] = useState<InputPropsType['type']>('default')
+
+  const { data } = useAppSelector((state) => state.user)
+  const dispatch = useAppDispatch()
+
+  const { changeData } = userSlice.actions
+  const { hide } = drawerSlice.actions
+
+  const text = `Обновить ${type !== 'Фамилия' ? type?.toLowerCase() : 'фамилию'}`
+  const disabled = data[key].length === 0 ? true : false
+
+  const handlerChangeData = (e: React.ChangeEvent<HTMLInputElement>) =>
+    dispatch(changeData({ ...data, [key]: e.target.value }))
+
+  useEffect(() => {
+    if (isSuccess) {
+      dispatch(hide())
+    }
+  }, [isSuccess])
+
+  useMemo(() => {
+    if (type === 'Имя') {
+      setKey('firstname')
+      setMessage('')
+      setInputType('default')
+    }
+    if (type === 'Фамилия') {
+      setKey('lastname')
+      setMessage('')
+      setInputType('default')
+    }
+    if (type === 'Роль') {
+      setKey('role')
+      setMessage('')
+      setInputType('default')
+    }
+    if (type === 'Логин') {
+      setKey('login')
+      setMessage('')
+      setInputType('default')
+    }
+    if (type === 'Пароль') {
+      setKey('password')
+      setMessage('')
+      setInputType('default')
+    }
+  }, [type, isError])
+
+  useMemo(() => {
+    if (isError) {
+      setInputType('error')
+
+      if (type === 'Имя') {
+        setMessage('Ошибка: невалидное имя')
+      }
+      if (type === 'Фамилия') {
+        setMessage('Ошибка: невалидная фамилия')
+      }
+      if (type === 'Роль') {
+        setMessage('Ошибка: роль может быть "Спортсмен" или "Тренер"')
+      }
+      if (type === 'Логин') {
+        setMessage('Ошибка: такой логин уже существует')
+      }
+      if (type === 'Пароль') {
+        setMessage('Ошибка: невалидный пароль')
+      }
+    }
+    if (!isError) {
+      setMessage('')
+      setInputType('default')
+    }
+  }, [isError])
+
+  return { key, message, inputType, text, data, type, disabled, handlerChangeData }
+}
