cc4b201152cffe9ee9ebd57bb3a5fbba802f7b53
Nikita <zharinov.nicita@gmail.com>
2022-08-02 11:01:59 +0700

    feature/registration authorization service

---
+++ src/redux/authorization/authorization.service.ts
@@ -0,0 +1,43 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export type UserType = {
+  _id: string
+  firstname: string
+  lastname: string
+  role: string
+  login: string
+  password: string
+  plans: Array<string>
+}
+
+export type UserAuthorizationType = {
+  firstname: string
+  lastname: string
+  role: string
+  login: string
+  password: string
+}
+
+export const authorizationApi = createApi({
+  tagTypes: ['Authorization'],
+  reducerPath: 'authorizationApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: String(process.env['REACT_APP_API_BASE_URL']),
+    prepareHeaders: (headers, { getState }) => {
+      headers.set('api-key', String(process.env['REACT_APP_API_KEY']))
+      return headers
+    },
+  }),
+  endpoints: (builder) => ({
+    authorization: builder.mutation<UserType, UserAuthorizationType>({
+      query: (user) => ({
+        url: `/user/authorization`,
+        method: 'POST',
+        body: user,
+      }),
+      invalidatesTags: ['Authorization'],
+    }),
+  }),
+})
+
+export const { useAuthorizationMutation } = authorizationApi
